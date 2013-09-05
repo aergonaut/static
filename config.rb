bb7d9c86bb9ca1f5ccc4a6ebe88b9acd6a909eee
Chris Fung <aergonaut@gmail.com>
2013-09-05 12:19:50 -0700

    entering ciran grammar

--- config.rb
+++ config.rb
@@ -51,6 +51,9 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+set :markdown, :tables => true, :no_instra_emphasis => true
+set :markdown_engine, :redcarpet
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
